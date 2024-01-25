db216abdf49f8ec5e7d222816a110e61cffb7ee4
Kashish patil <156797313+kashishpatil1218@users.noreply.github.com>
2024-01-25 10:57:54 +0530

    Add files via upload

---
+++ PRACTICAL 1/1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	printf("Name      :kashish Patil\n");
+	printf("Age       :18\n");
+	printf("Mobile num:9724212106\n");
+	printf("Addres    :B-652,Millenium park,Dindoli\n");
+	printf("study     :At RNW miltimedia\n");
+	
+}
