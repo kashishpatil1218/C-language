e9fab8e85b12494cf5e085c7874165555f037bde
Kashish patil <156797313+kashishpatil1218@users.noreply.github.com>
2024-02-18 20:32:55 +0530

    Add files via upload

---
+++ stick 3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("Enter the First num :");
+	scanf("%d",&a);
+	int b;
+	printf("Enter the Sec num :");
+	scanf("%d",&b);
+
+	int x[a];
+	
+	
+	int i;
+	
+	for(i=a; i<=b; i++)
+	{
+		
+		if(i%4==0)
+		{
+			printf("%d ",i);
+		}
+	
+	}
+
+}
